de7794cbe9c80709c631041f685a3855061a403f
didtheyghostme <didtheyghostme@gmail.com>
2024-10-27 22:35:46 +0800

    add custom item to clerk user button navbar sign in/out
    - create <ClerkUserButton />
    - default label "manageAccount" | "signOut"
    - custom button by wrapping button in <SignInButton>
    - TODO: add custom "View my applications"
    
    https://clerk.com/docs/customization/user-button
    https://clerk.com/docs/components/unstyled/sign-in-button

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,32 +1,15 @@
+"use client";
+
 import NextLink from "next/link";
 import clsx from "clsx";
-import {
-  Button,
-  Kbd,
-  Link,
-  Input,
-  link as linkStyles,
-  Navbar as NextUINavbar,
-  NavbarContent,
-  NavbarMenu,
-  NavbarMenuToggle,
-  NavbarBrand,
-  NavbarItem,
-  NavbarMenuItem,
-} from "@nextui-org/react";
-import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/nextjs";
+import { Button, Kbd, Link, Input, link as linkStyles, Navbar as NextUINavbar, NavbarContent, NavbarMenu, NavbarBrand, NavbarItem, NavbarMenuItem } from "@nextui-org/react";
+import { SignedIn, SignedOut, SignInButton } from "@clerk/nextjs";
+
+import { ClerkUserButton } from "./ClerkUserButton";
 
 import { siteConfig } from "@/config/site";
 import { ThemeSwitch } from "@/components/theme-switch";
-import {
-  TwitterIcon,
-  GithubIcon,
-  DiscordIcon,
-  HeartFilledIcon,
-  SearchIcon,
-  Logo,
-} from "@/components/icons";
-
+import { TwitterIcon, GithubIcon, DiscordIcon, HeartFilledIcon, SearchIcon, Logo } from "@/components/icons";
 
 export const Navbar = () => {
   const searchInput = (
@@ -34,6 +17,7 @@ export const Navbar = () => {
       aria-label="Search"
       labelPlacement="outside"
       placeholder="Search..."
+      startContent={<SearchIcon className="pointer-events-none flex-shrink-0 text-base text-default-400" />}
       type="search"
       classNames={{
         inputWrapper: "bg-default-100",
@@ -43,9 +27,6 @@ export const Navbar = () => {
         <Kbd className="hidden lg:inline-block" keys={["command"]}>
           K
         </Kbd>
-      }
-      startContent={
-        <SearchIcon className="pointer-events-none flex-shrink-0 text-base text-default-400" />
       }
     />
   );
@@ -62,14 +43,7 @@ export const Navbar = () => {
         <ul className="ml-2 hidden justify-start gap-4 lg:flex">
           {siteConfig.navItems.map((item) => (
             <NavbarItem key={item.href}>
-              <NextLink
-                color="foreground"
-                href={item.href}
-                className={clsx(
-                  linkStyles({ color: "foreground" }),
-                  "data-[active=true]:font-medium data-[active=true]:text-primary",
-                )}
-              >
+              <NextLink className={clsx(linkStyles({ color: "foreground" }), "data-[active=true]:font-medium data-[active=true]:text-primary")} color="foreground" href={item.href}>
                 {item.label}
               </NextLink>
             </NavbarItem>
@@ -77,10 +51,7 @@ export const Navbar = () => {
         </ul>
       </NavbarContent>
 
-      <NavbarContent
-        className="hidden basis-1/5 sm:flex sm:basis-full"
-        justify="end"
-      >
+      <NavbarContent className="hidden basis-1/5 sm:flex sm:basis-full" justify="end">
         <NavbarItem className="hidden gap-2 sm:flex">
           <Link isExternal aria-label="Twitter" href={siteConfig.links.twitter}>
             <TwitterIcon className="text-default-500" />
@@ -95,7 +66,7 @@ export const Navbar = () => {
         </NavbarItem>
         <NavbarItem className="hidden lg:flex">{searchInput}</NavbarItem>
         <NavbarItem className="hidden md:flex">
-          <Button
+          {/* <Button
             isExternal
             as={Link}
             className="bg-default-100 text-sm font-normal text-default-600"
@@ -104,12 +75,16 @@ export const Navbar = () => {
             variant="flat"
           >
             Sponsor
-          </Button>
+          </Button> */}
           <SignedOut>
-            <SignInButton />
+            <SignInButton>
+              <Button className="bg-default-100 text-sm font-normal text-default-600" variant="flat">
+                Login
+              </Button>
+            </SignInButton>
           </SignedOut>
           <SignedIn>
-            <UserButton />
+            <ClerkUserButton />
           </SignedIn>
         </NavbarItem>
       </NavbarContent>
@@ -119,7 +94,19 @@ export const Navbar = () => {
           <GithubIcon className="text-default-500" />
         </Link>
         <ThemeSwitch />
-        <NavbarMenuToggle />
+
+        <SignedOut>
+          <SignInButton>
+            <Button className="bg-default-100 text-sm font-normal text-default-600" startContent={<HeartFilledIcon className="text-danger" />} variant="flat">
+              Login
+            </Button>
+          </SignInButton>
+        </SignedOut>
+
+        <SignedIn>
+          <ClerkUserButton />
+        </SignedIn>
+        {/* <NavbarMenuToggle /> */}
       </NavbarContent>
 
       <NavbarMenu>
@@ -127,17 +114,7 @@ export const Navbar = () => {
         <div className="mx-4 mt-2 flex flex-col gap-2">
           {siteConfig.navMenuItems.map((item, index) => (
             <NavbarMenuItem key={`${item}-${index}`}>
-              <Link
-                href="#"
-                size="lg"
-                color={
-                  index === 2
-                    ? "primary"
-                    : index === siteConfig.navMenuItems.length - 1
-                      ? "danger"
-                      : "foreground"
-                }
-              >
+              <Link color={index === 2 ? "primary" : index === siteConfig.navMenuItems.length - 1 ? "danger" : "foreground"} href="#" size="lg">
                 {item.label}
               </Link>
             </NavbarMenuItem>
