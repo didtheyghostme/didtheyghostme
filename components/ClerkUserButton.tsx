44cf033bf11c3a073c9fa1ad7eb7e3cf54efc3a8
didtheyghostme <didtheyghostme@gmail.com>
2024-11-08 15:25:36 +0800

    add mixpanel tracking of view my applications clicked

--- components/ClerkUserButton.tsx
+++ components/ClerkUserButton.tsx
@@ -1,18 +1,24 @@
 "use client";
 
 import { UserButton } from "@clerk/nextjs";
-import { useRouter, usePathname } from "next/navigation";
+import { useRouter } from "next/navigation";
 import mixpanel from "mixpanel-browser";
 
 import { ApplicationsIcon, ContactIcon } from "./icons";
 
 export const ClerkUserButton = () => {
   const router = useRouter();
-  const pathname = usePathname();
+
+  const handleViewMyApplicationsClick = () => {
+    mixpanel.track("View My Applications Clicked", {
+      placement: "ClerkUserButton navbar /applications",
+    });
+
+    router.push("/applications");
+  };
 
   const handleContactSupportClick = () => {
     mixpanel.track("Contact Support Clicked", {
-      pathname,
       placement: "ClerkUserButton navbar /contact",
     });
 
@@ -22,8 +28,8 @@ export const ClerkUserButton = () => {
   return (
     <UserButton>
       <UserButton.MenuItems>
-        <UserButton.Link href="/applications" label="View my job applications" labelIcon={<ApplicationsIcon />} />
-        <UserButton.Action label="Contact Support" labelIcon={<ContactIcon />} onClick={() => handleContactSupportClick()} />
+        <UserButton.Action label="View my job applications" labelIcon={<ApplicationsIcon />} onClick={handleViewMyApplicationsClick} />
+        <UserButton.Action label="Contact Support" labelIcon={<ContactIcon />} onClick={handleContactSupportClick} />
         <UserButton.Action label="manageAccount" />
         <UserButton.Action label="signOut" />
       </UserButton.MenuItems>
