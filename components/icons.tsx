18a1f96776d6c5e39400fa0577591a9411596afa
didtheyghostme <didtheyghostme@gmail.com>
2024-11-11 18:40:46 +0800

    fix npm run build warnings
    eslint-disable max-len for svg icons.tsx

--- components/icons.tsx
+++ components/icons.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable max-len */
 import * as React from "react";
 
 import { IconSvgProps } from "@/types";
@@ -165,7 +166,7 @@ export const ContactIcon: React.FC<IconSvgProps> = ({ size = 15, width, height, 
 );
 
 export const AlertCircleIcon = ({ size = 24, width, height, className = "", ...props }: IconSvgProps) => (
-  <svg aria-hidden="true" fill="none" focusable="false" height={size || height} role="presentation" viewBox="0 0 24 24" width={size || width} className={className} {...props}>
+  <svg aria-hidden="true" className={className} fill="none" focusable="false" height={size || height} role="presentation" viewBox="0 0 24 24" width={size || width} {...props}>
     <path
       d="M12 22C17.5228 22 22 17.5228 22 12C22 6.47715 17.5228 2 12 2C6.47715 2 2 6.47715 2 12C2 17.5228 6.47715 22 12 22Z"
       stroke="currentColor"
--- components/primitives.ts
+++ components/primitives.ts
@@ -27,15 +27,7 @@ export const title = tv({
   },
   compoundVariants: [
     {
-      color: [
-        "violet",
-        "yellow",
-        "blue",
-        "cyan",
-        "green",
-        "pink",
-        "foreground",
-      ],
+      color: ["violet", "yellow", "blue", "cyan", "green", "pink", "foreground"],
       class: "bg-clip-text text-transparent bg-gradient-to-b",
     },
   ],
