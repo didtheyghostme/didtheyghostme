1c67149d54c111106cd0da0af2c4bf77da13173f
didtheyghostme <didtheyghostme@gmail.com>
2024-12-07 15:38:39 +0800

    add setting page to ClerkUserButton dropdown

--- components/ClerkUserButton.tsx
+++ components/ClerkUserButton.tsx
@@ -4,7 +4,7 @@ import { UserButton } from "@clerk/nextjs";
 import { useRouter } from "next/navigation";
 import mixpanel from "mixpanel-browser";
 
-import { ApplicationsIcon, ContactIcon } from "./icons";
+import { ApplicationsIcon, ContactIcon, EditIcon } from "./icons";
 
 export const ClerkUserButton = () => {
   const router = useRouter();
@@ -25,11 +25,20 @@ export const ClerkUserButton = () => {
     router.push("/contact");
   };
 
+  const handleSettingsClick = () => {
+    mixpanel.track("Default Settings Clicked", {
+      component: "ClerkUserButton navbar /settings",
+    });
+
+    router.push("/settings");
+  };
+
   return (
     <UserButton>
       <UserButton.MenuItems>
         <UserButton.Action label="View my job applications" labelIcon={<ApplicationsIcon />} onClick={handleViewMyApplicationsClick} />
         <UserButton.Action label="Contact Support" labelIcon={<ContactIcon />} onClick={handleContactSupportClick} />
+        <UserButton.Action label="Default Settings" labelIcon={<EditIcon />} onClick={handleSettingsClick} />
         <UserButton.Action label="manageAccount" />
         <UserButton.Action label="signOut" />
       </UserButton.MenuItems>
