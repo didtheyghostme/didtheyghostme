bdea43f7d8be6dbf5a1356623629fd888e556ac3
didtheyghostme <didtheyghostme@gmail.com>
2026-01-14 01:31:55 +0800

    fix npm run build error

--- lib/readme-sync/exportSgInternTechVerifiedJobs.ts
+++ lib/readme-sync/exportSgInternTechVerifiedJobs.ts
@@ -18,11 +18,11 @@ function hasCountry(job: ExportJobRow, countryName: string): boolean {
   return job.job_posting_country?.some((x) => x.country.country_name === countryName) ?? false;
 }
 
-function hasExperienceLevel(job: ExportJobRow, experienceLevel: string): boolean {
+function hasExperienceLevel(job: ExportJobRow, experienceLevel: ExperienceLevel): boolean {
   return job.job_posting_experience_level?.some((x) => x.experience_level.experience_level === experienceLevel) ?? false;
 }
 
-function hasJobCategory(job: ExportJobRow, jobCategory: string): boolean {
+function hasJobCategory(job: ExportJobRow, jobCategory: JobCategoryName): boolean {
   return job.job_posting_job_category?.some((x) => x.job_category.job_category_name === jobCategory) ?? false;
 }
 
@@ -51,18 +51,21 @@ export async function exportSgInternTechVerifiedJobs(): Promise<ExportReadmeJob[
     [DBTable.JOB_POSTING_COUNTRY]: {
       __isLeftJoin: true,
       [DBTable.COUNTRY]: {
+        id: true,
         country_name: true,
       },
     },
     [DBTable.JOB_POSTING_EXPERIENCE_LEVEL]: {
       __isLeftJoin: true,
       [DBTable.EXPERIENCE_LEVEL]: {
+        id: true,
         experience_level: true,
       },
     },
     [DBTable.JOB_POSTING_JOB_CATEGORY]: {
       __isLeftJoin: true,
       [DBTable.JOB_CATEGORY]: {
+        id: true,
         job_category_name: true,
       },
     },
